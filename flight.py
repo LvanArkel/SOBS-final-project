9ea0dd31aabf9e05418f0e9e32bb0577ce15b798
Gies <giesdb@gmail.com>
2023-02-01 15:50:37 +0100

    salto

--- flight.py
+++ flight.py
@@ -8,18 +8,23 @@ def flightparms(system):
     stiffness = 50
     damping = 10
     frequency = 1.7
-    hip_moment_mult = 50
-    shoulder_moment_mult = 30
-    shoulder_angle = -0.25 * np.pi
-    hip_angle = -0.25 * np.pi
+    hip_moment_mult = 30
+    shoulder_moment_mult = 20
+    knee_moment_mult = 30
+
+    knee_angle = -0.5 * np.pi
+    shoulder_angle = -0 * np.pi
+    hip_angle = -0.5 * np.pi
     swingparms = {
         'segparms': segparms,
         'stiffness': stiffness,
         'damping': damping,
         'hip_moment_mult': hip_moment_mult,
         'shoulder_moment_mult': shoulder_moment_mult,
+        'knee_moment_mult': knee_moment_mult,
         'shoulder_angle': shoulder_angle,
-        'hip_angle': hip_angle
+        'hip_angle': hip_angle,
+        'knee_angle': knee_angle,
     }
     return swingparms
 
@@ -27,12 +32,22 @@ def flightparms(system):
 
 
 def flightstate(system):
-    swingstate = [-0.9121063 , -0.85812016, -0.85011183, -0.85049494, -0.85832449,
-       -1.04205367, -0.12453037, -0.51850652,  0.50177495,  0.67365286,
-        0.7051935 ,  0.68995334,  0.59797652,  0.46833777,  1.26711656,
-        1.33613255,  0.        ,  8.        ,  0.        ,  0.        ]
-    swingbase_pos = [1.29272637, 6.47481468]
-    swingbase_vel = [0.97746026, 0.83402999]
+    # LARS
+    # swingstate = [-0.9121063 , -0.85812016, -0.85011183, -0.85049494, -0.85832449,
+    #    -1.04205367, -0.12453037, -0.51850652,  0.50177495,  0.67365286,
+    #     0.7051935 ,  0.68995334,  0.59797652,  0.46833777,  1.26711656,
+    #     1.33613255,  0.        ,  8.        ,  0.        ,  0.        ]
+    # swingbase_vel = [0.97746026, 0.83402999]
+    # swingbase_pos = [1.29272637, 6.47481468]
+
+    # GIES
+    swingstate = [-1.86503057, -1.89215125, -1.892467  , -1.89009032, -1.89254894,
+       -1.93107123, -1.76578462, -1.7960798 ,  0.57694302,  0.63301128,
+        0.62706037,  0.66608912,  0.96329365,  2.06334383,  6.34132209,
+        6.88543933,  0.        ,  0.        ,  0.        ,  0.        ]
+    swingbase_vel = [1.19003784, -0.3872925]
+    swingbase_pos = [0, 11]
+
     segdynstate = swingstate_to_flight_state(swingstate, swingbase_pos, swingbase_vel)
      # Ms = np.zeros(rope_segments)
     state = segdynstate
@@ -46,6 +61,8 @@ def flightshell(t, state, parms):
     damping = parms['damping']
     hip_moment_mult = parms['hip_moment_mult']
     shoulder_moment_mult = parms['shoulder_moment_mult']
+    knee_moment_mul = parms['knee_moment_mult']
+    knee_angle = parms['knee_angle']
     shoulder_angle = parms['shoulder_angle']
     hip_angle = parms['hip_angle']
 
@@ -70,16 +87,26 @@ def flightshell(t, state, parms):
     # hip_moment = np.sin(frequency * t) * hip_moment_mult
     # hip_moment = rope_ang * hip_moment_mult
     # hip_moment = rope_vel * hip_moment_mult
-    current_shoulder_angle = phis[1] - phis[0]
-    current_hip_angle = phis[2] - phis[1]
+    curr_s_angle = phis[1] - phis[0]
+    curr_h_angle = phis[2] - phis[1]
+    curr_k_angle = phis[3] - phis[2]
+    # LARS
+    # Ms[1] += (shoulder_angle - current_shoulder_angle) * shoulder_moment_mult
+    # Ms[2] += (hip_angle - current_hip_angle) * hip_moment_mult
 
-    Ms[1] += (shoulder_angle - current_shoulder_angle) * shoulder_moment_mult
-    Ms[2] += (hip_angle - current_hip_angle) * hip_moment_mult
+    # GIES
+    shoulder_diff = shoulder_angle - curr_s_angle
+    Ms[1] += curr_s_angle * shoulder_moment_mult * 3
+    hip_diff = hip_angle - curr_h_angle
+    Ms[2] += hip_diff * hip_moment_mult
+    knee_diff = knee_angle - curr_k_angle
+    Ms[3] += knee_diff * knee_moment_mul
+    print(f"Angles Shoulder {curr_s_angle}, Hip {curr_h_angle}, Knee {curr_k_angle}")
+    print(f"Angle Diffs Shoulder {shoulder_diff}, Hip {hip_diff}, Knee {knee_diff}")
     # print(f"Moment {hip_moment}, Rope Ang {rope_ang}, Rope Acc {rope_acc}")
 
-
     # V 7 * nseg + 5
-    Fx = np.concatenate( (np.array([0]), np.full(nseg - 1, np.nan), np.array([0])))        # Fx nseg + 1,
+    Fx = np.concatenate((np.array([0]), np.full(nseg - 1, np.nan), np.array([0])))        # Fx nseg + 1,
     Fy = np.concatenate((np.array([0]), np.full(nseg - 1, np.nan), np.array([0])))       # Fy nseg + 1,
     M = np.concatenate((Ms, np.zeros(1)))   # M nseg + 1,
     Fextx = np.zeros(nseg)    # Fextx nseg,
@@ -100,11 +127,17 @@ def flightshell(t, state, parms):
 
 def collision_event(t, state, parms):
     segparms = parms['segparms']
-    (_, jointy), *_ = jointcoord(state, segparms)
+    (jointx, jointy), *_ = jointcoord(state, segparms)
+    print(f"Jointx {jointx},  Jointy, {jointy}")
     criterion = jointy.min()
+    print(f"Criterion {criterion}")
     if criterion < 1 and criterion > -1:
         print(criterion)
     return criterion
+
+def gies_collision_event(t, state, parms):
+    segparms = parms['segparms']
+    (jointx, jointy), *_ = jointcoord(state, segparms)
 
 def flight(system):
     initial_state = flightstate(system)
@@ -113,7 +146,7 @@ def flight(system):
 
     t_span = [0, 2]
     ODE = lambda t, state, parms: flightshell(t, state, parms)[0]
-    collision_event.terminal = True
+    # collision_event.terminal = True
 
     sol = integrate.solve_ivp(ODE, t_span, initial_state, args=(parms,), events=collision_event, rtol=1e-8, atol=1e-8)
 
@@ -155,4 +188,4 @@ if __name__ == "__main__":
     # phis, phids, base_pos, base_vel = readsegdynstate(state0, len(system))
     # print(f"Phis: {phis}")
 
-    our_animate(sol.t, sol.y, segparms, axlim=7)
+    our_animate(sol.t, sol.y, segparms, axlim=13)
